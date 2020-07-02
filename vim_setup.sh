21d8720df5fd7d4c4163b3a3165763dc0c8d1b0c
Zaki Ghouse <zghouse7@gmail.com>
2020-07-01 21:35:15 -0500

    Install vim plugins

---
+++ vim_setup.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+mkdir -p ~/.vim/pack/plugins/start
+git clone https://github.com/morhetz/gruvbox.git ~/.vim/pack/plugins/start/gruvbox
+git clone https://github.com/rust-lang/rust.vim ~/.vim/pack/plugins/start/rust.vim
+git clone https://github.com/edkolev/tmuxline.vim.git ~/.vim/pack/plugins/start/tmuxline.vim
+git clone https://github.com/vim-airline/vim-airline.git ~/.vim/pack/plugins/start/vim-airline
+git clone https://github.com/vim-airline/vim-airline-themes.git ~/.vim/pack/plugins/start/vim-airline-themes
+git clone https://github.com/fatih/vim-go.git ~/.vim/pack/plugins/start/vim-go
