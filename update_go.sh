251cbd92530dba58d6dc74c45396226d219b751a
Zaki Ghouse <zghouse7@gmail.com>
2020-07-14 21:56:07 -0500

    exit go update script if no version provided

--- update_go.sh
+++ update_go.sh
@@ -1,4 +1,10 @@
 VERSION=$1
+
+if [[ -z $VERSION ]]; then
+  echo "Please provide Go version to update or use"
+  exit 1
+fi
+
 if [[ -x $(command -v go) ]] ; then
   go get golang.org/dl/go$VERSION
   if [[ $? != 0 ]]; then
