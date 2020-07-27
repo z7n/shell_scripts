dd7ff5339efde42fd8819ec22a5f3ed6538a5149
Zaki Ghouse <zghouse7@gmail.com>
2020-07-26 19:43:14 -0500

    add ubuntu packages

--- install_apps.sh
+++ install_apps.sh
@@ -2,3 +2,6 @@
 if [[ -f /etc/fedora-release ]]; then
   dnf install tmux vim keychain jq fish hub git tig gnome-tweaks gnome-shell-extension-{pomodoro,openweather} buildah podman
 fi
+if [[ -f /etc/lsb-release ]]; then
+  apt install tmux vim keychain jq git tig gnome-tweaks fonts-powerline lxc lxd gnome-shell-pomodoro
+fi
